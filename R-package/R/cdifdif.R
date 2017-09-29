957ec4f5734bc4aa7d8590227aaf9ea9b2716782
Joshua Kunst <jbkunst@gmail.com>
2017-09-29 03:11:50 -0300

    adding DESCRIPTION info

---
+++ R-package/R/cdifdif.R
@@ -0,0 +1,9 @@
+#' cdifdif: Spillover Robust Difference-in-Differences Estimation
+#'
+#' This package implements spillover robust difference-in-differences
+#' estimates as described in Clarke (2017).  It tests for local spillovers in a
+#' diff-in-diff setting, and optimally determines spillover propagation.
+#' It returns estimates of both the treatment effect, as well as (if relevant)
+#' the treatment effect on those close to treated, who weren't themselves treated.
+#'
+"_PACKAGE"
